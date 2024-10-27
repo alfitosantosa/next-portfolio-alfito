ed76064f0646ffe924473847f6a5fc786c9e8c8b
alfitosantosa <sanskun02pg@gmail.com>
2024-10-28 05:41:07 +0700

    fixing bg retro grid fixed

--- components/ui/retro-grid.tsx
+++ components/ui/retro-grid.tsx
@@ -2,14 +2,14 @@ import { cn } from "@/lib/utils";
 
 export default function RetroGrid({ className, angle = 65 }: { className?: string; angle?: number }) {
   return (
-    <div className={cn("pointer-events-none absolute size-full overflow-hidden opacity-50 [perspective:200px]", className)} style={{ "--grid-angle": `${angle}deg` } as React.CSSProperties}>
+    <div className={cn("pointer-events-none absolute size-full opacity-100 [perspective:300px]", className)} style={{ "--grid-angle": `${angle}deg` } as React.CSSProperties}>
       {/* Grid */}
       <div className="absolute inset-0 [transform:rotateX(var(--grid-angle))]">
         <div
           className={cn(
             "animate-grid",
 
-            "[background-repeat:repeat] [background-size:60px_60px] [height:300vh] [inset:0%_0px] [margin-left:-50%] [transform-origin:100%_0_0] [width:600vw]",
+            "[background-repeat:repeat] [background-size:60px_60px] [height:500vh] [inset:0%_0px] [margin-left:-50%] [transform-origin:100%_0_0] [width:600vw]",
 
             // Light Styles
             "[background-image:linear-gradient(to_right,rgba(0,0,0,0.3)_1px,transparent_0),linear-gradient(to_bottom,rgba(0,0,0,0.3)_1px,transparent_0)]",
@@ -21,7 +21,7 @@ export default function RetroGrid({ className, angle = 65 }: { className?: strin
       </div>
 
       {/* Background Gradient */}
-      <div className="absolute inset-0 bg-gradient-to-t from-white to-transparent to-10% dark:from-black" />
+      {/* <div className="absolute inset-0 bg-gradient-to-t from-white to-transparent to-10% dark:from-black" /> */}
     </div>
   );
 }
